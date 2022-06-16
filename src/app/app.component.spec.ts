6aba94adb31bd86d6e6ab415ee6609779ffe510c
Brian Kingrey <brian@godo.io>
2022-06-16 17:49:23 -0600

    Angulor ngrx init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Ngrx-Gaming-Template'`, () => {
+  it(`should have as title 'Angulore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Ngrx-Gaming-Template');
+    expect(app.title).toEqual('Angulore');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Ngrx-Gaming-Template app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Angulore app is running!'
+    );
   });
 });
